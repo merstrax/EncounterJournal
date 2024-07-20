5ff89065c0e37bb558eecf67b070a96a25d5a0e1
Michael Amundson <merstrax@gmail.com>
2024-07-20 15:44:02 -0500

    Added missing Diremaul bosses

--- Data/Classic/Dungeons/EJ_Diremaul.lua
+++ Data/Classic/Dungeons/EJ_Diremaul.lua
@@ -8,47 +8,47 @@ instance:setAttributes(230, "Dire Maul", EJ_Descriptions.Instances["DIREMAUL"], 
 local zevrim_thornhoof = EJ_Data:CreateEncounter();
 zevrim_thornhoof:setAttributes("Zevrim Thornhoof", "Desperate to show his worth to the Burning Legion, Zevrim Thornhoof told his masters he could enslave the formidable ancients who still roamed Dire Maul. When his own power proved insufficient, Thornhoof turned to other living creatures for additional energy, butchering them atop his sacrificial altar and harvesting their essence in their final, agonizing moments.", 230, 343, 1, 4);
 zevrim_thornhoof:setLoot({18319,18313,18323,18306,18308}); 
-instance:addEncounter(); 
+instance:addEncounter(zevrim_thornhoof); 
 
 local hydrospawn = EJ_Data:CreateEncounter();
 hydrospawn:setAttributes("Hydrospawn", "When the satyr Zevrim Thornhoof sought to control the elementals in Dire Maul, he focused on Hydrospawn, who calmly resided in the pool beneath his altar. As he performed his profane sacrificial rituals, the elemental grew agitated and restless, striking out at Thornhoof's demonic minions. As the years passed, Thornhoof used his victims' energies to whip Hydrospawn into unbridled fury, little by little binding its will to the satyr's commands.", 230, 344, 2, 4);
 hydrospawn:setLoot({18322,18317,18324,18307,18305}); 
-instance:addEncounter(); 
+instance:addEncounter(hydrospawn); 
 
 local lethtendris = EJ_Data:CreateEncounter();
 lethtendris:setAttributes("Lethtendris", "Dire Maul's warped energy drew Lethtendris from afar, giving her hope of satisfying her unbearable craving for arcane magic. Using the sinews and ligaments of sacrificial victims, she constructed a web to siphon the corrupt power out of the east wing. To her delight, she discovered that the device has the capacity to gather vast reserves of overwhelming strength, enough to destroy all of her enemies.", 230, 345, 3, 4);
 lethtendris:setLoot({18325,18311,18302}); 
-instance:addEncounter(); 
+instance:addEncounter(lethtendris); 
 
 local alzzin_the_wildshaper = EJ_Data:CreateEncounter();
 alzzin_the_wildshaper:setAttributes("Alzzin the Wildshaper", "Alzzin the Wildshaper seeks nothing less than the total corruption of Feralas, but until he discovered the Shrine of Eldretharr in Dire Maul, he had little success. With the power of enslaved ancients at his command, Alzzin concentrated the vitality of the surrounding forest into his lair, allowing him to manipulate it at will.", 230, 346, 4, 4);
 alzzin_the_wildshaper:setLoot({18328,18327,18309,18318,18312,18326}); 
-instance:addEncounter(); 
+instance:addEncounter(alzzin_the_wildshaper); 
 
 local tendris_warpwood = EJ_Data:CreateEncounter();
 tendris_warpwood:setAttributes("Tendris Warpwood", "Tendris Warpwood was a staunch defender of Eldre'Thalas, his heart inexorably entwined with the fate of the city. When the Sundering shattered the land, his resolve never wavered, but as corruption seeped into the ruins over the next 10,000 years, so too did it infect the ancient's spirit.", 230, 350, 5, 4);
 tendris_warpwood:setLoot({18390,18393,18353,18352}); 
-instance:addEncounter(); 
+instance:addEncounter(tendris_warpwood); 
 
 local illyanna_ravenoak = EJ_Data:CreateEncounter();
 illyanna_ravenoak:setAttributes("Illyanna Ravenoak", "Illyanna Ravenoak, along with her faithful companion Ferra, was once Prince Tortheldrin's most valued hunter, until she refused to carry out her leader's order to execute her friends. As punishment, he killed Illyanna and her bear, but he used his demonic power to bind their spirits to their bodies, condemning them to remain in Dire Maul as undead protectors.", 230, 347, 6, 4);
 illyanna_ravenoak:setLoot({18386,18383,18349,18347}); 
-instance:addEncounter(); 
+instance:addEncounter(illyanna_ravenoak); 
 
 local magister_kalendris = EJ_Data:CreateEncounter();
 magister_kalendris:setAttributes("Magister Kalendris", "When Prince Tortheldrin demanded the slaughter of the majority of his subjects, his most loyal lieutenants were tasked with executing his will. Magister Kalendris murdered many unsuspecting Shen'dralar without hesitation, but one of the doomed elves mortally wounded him during a destructive battle. Despite his demise, Kalendris's spirit still roams Dire Maul as a defender of Tortheldrin's rule.", 230, 348, 7, 4);
 magister_kalendris:setLoot({18374,18397,18371,18350,18351}); 
-instance:addEncounter(); 
+instance:addEncounter(magister_kalendris); 
 
 local immol_thar = EJ_Data:CreateEncounter();
 immol_thar:setAttributes("Immol'thar", "In a desperate bid to restore the power and immortality they lost when the Well of Eternity was destroyed, the Shen'dralar summoned a demon into the western wing of Dire Maul and captured him within a force field, allowing them to use the void terror's energy for their own spells. Unable to fight back or even move for nearly 10,000 years, Immol'thar could only watch and wait for the day he might break free and rend the flesh of his captors.", 230, 349, 8, 4);
 immol_thar:setLoot({18389,18385,18377,18391,18394,18379,18384,18381,18370,18372}); 
-instance:addEncounter(); 
+instance:addEncounter(immol_thar); 
 
 local prince_tortheldrin = EJ_Data:CreateEncounter();
 prince_tortheldrin:setAttributes("Prince Tortheldrin", "After the Well of Eternity's destruction, Prince Tortheldrin maintained control over the surviving Shen'dralar by imprisoning a demon in the ruins of Eldre'Thalas and letting his subjects siphon its strength. The exposure to demonic power twisted Tortheldrin's mind, and when the creature faltered, the prince ordered the deaths of many of his vassals so only he and his most fanatical followers would be left to enjoy the corrupt energy.", 230, 361, 9, 4);
 prince_tortheldrin:setLoot({18382,18373,18375,18378,18380,18395}); 
-instance:addEncounter(); 
+instance:addEncounter(prince_tortheldrin); 
 
 local guard_mol_dar = EJ_Data:CreateEncounter();
 guard_mol_dar:setAttributes("Guard Mol'dar", "Mol'dar is a zealously loyal guard from the Gordok ogres. He is convinced King Gordok gifted him with a pair of enchanted gauntlets that have enabled him to become his people's arm wrestling champion for six years running. In truth, King Gordok threw the powerful armor away; Mol'dar simply found it first.", 230, 362, 10, 4);
